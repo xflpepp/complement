ff30cc2218e4c0c051bd22f23d7d6240286d4103
xflpepp <84723182+xflpepp@users.noreply.github.com>
2021-05-26 15:04:41 -0400

    Add files via upload

---
+++ low graphics.lua
@@ -0,0 +1,11 @@
+for _,v in pairs(workspace:GetDescendants()) do
+if v.ClassName == "Part"
+or v.ClassName == "SpawnLocation"
+or v.ClassName == "WedgePart"
+or v.ClassName == "Terrain"
+or v.ClassName == "MeshPart" then
+v.Material = "Plastic"
+game:GetService("Lighting").GlobalShadows = false
+
+end
+end
